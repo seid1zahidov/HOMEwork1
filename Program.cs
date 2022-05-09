38655327579d491410d99fdddc5503725cf4b2ea
Seid <101628221+seid1zahidov@users.noreply.github.com>
2022-05-09 10:48:30 -0700

    task1

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using HOMEwork.Models;
+
+namespace HOMEwork
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Library library = new Library();
+            library.books.Add(new Book("Seid", "Zahidov", 101));
+            library.books.Add(new Book("Nicat", " Muxtarov", 102));
+            //library.FindAllBooksByName("Seid");
+            //library.RemoveAllBookByName("Seid");
+            library.SearchBooks("Nicat");
+            foreach (Book item in library.books)
+            {
+                Console.WriteLine(item);
+            }
+
+        }
+    }
+}
